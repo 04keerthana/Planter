b0dc7cc024cab6da970bcc371c5e547917a53acc
04keerthana <121709095+04keerthana@users.noreply.github.com>
2023-02-02 20:59:30 +0530

    Add files via upload

---
+++ OnlinePlantApp1/src/main/java/com/cg/onlineplantnursery/exception/PlanterIdNotFoundException.java
@@ -0,0 +1,15 @@
+package com.cg.onlineplantnursery.exception;
+
+public class PlanterIdNotFoundException extends Exception{
+
+	public PlanterIdNotFoundException() {
+		
+	}
+	
+	public PlanterIdNotFoundException(String msg) {
+		super(msg);
+	}
+	
+	
+
+}
